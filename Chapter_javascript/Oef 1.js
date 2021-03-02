83f9d88069d76dc1b28358e98e082be74ca09114
Dries Bosmans <dries.bosmans@student.pxl.be>
2021-03-02 13:50:04 +0100

    oefening functies klaar

---
+++ Chapter_javascript/Oef 1.js
@@ -0,0 +1,6 @@
+
+let naam = prompt("geef je naam", "dries");
+
+document.getElementById('TitelNaam').innerHTML = naam;
+let dag =
+
---
+++ Chapter_javascript/link_voorbeeld.js
@@ -0,0 +1,3 @@
+let code = prompt('vul uw promotiecode in', 'uw code)');
+let tekst = 'de code die u invoerde was ' + code;
+document.getElementById( 'kortingscode').innerHTML = tekst;
