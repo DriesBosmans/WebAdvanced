264f6a3c289349886858906a5dd673618bad4cdc
DriesBosmans <73179981+DriesBosmans@users.noreply.github.com>
2021-05-18 14:24:37 +0200

    eerste commitm

---
+++ Chapter_vue/vue-crash-2021/src/components/Enter.vue
@@ -0,0 +1,52 @@
+<template>
+<div id="div1">
+<div id="inputs">
+  <input name="datum" id="datum">
+  <input name="task" id="task">
+</div>
+  <Button @click="$emit('addTask')" id="knop" class="btn" color="green" text="AddTask" ></Button>
+</div>
+</template>
+
+<script>
+import Button from "./Button";
+export default {
+name: "Enter",
+  components: {Button},
+
+  props:{
+
+
+  },
+  methods: {
+  addTask(){
+console.log('test')
+  }
+  }
+}
+</script>
+
+<style scoped>
+#div1{
+display: flex;
+  flex-direction: row;
+
+}
+input{width: 100%;
+display: block;
+border-radius: 4px;
+border: 1px black solid;
+margin: 10px;}
+
+#inputs{
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+}
+.btn {
+  margin: 10px;
+  background: #0f6674;
+}
+</style>
